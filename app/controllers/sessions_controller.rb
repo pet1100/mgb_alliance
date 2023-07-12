430f71ddf0ceedeb2ee467cab41d11d564e8cebd
Peter <peterbrj@gmail.com>
2023-07-12 20:14:53 +0200

    Added cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,18 @@
 class SessionsController < ApplicationController
   def new
-
+    if cookies[:user_uuid]
+      user = User.find_by(uuid: cookies[:user_uuid])
+      user.update last_used: Time.now
+      session[:user_id] = user.id
+      redirect_to home_path
+    end
   end
 
   def create
     user = User.find_or_create_by(uuid: params[:uuid])
     if user
       user.update last_used: Time.now
+      cookies[:user_uuid] = user.uuid
       session[:user_id] = user.id
       redirect_to home_path
     else
@@ -16,6 +22,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.clear
+    cookies.delete :user_uuid
     redirect_to root_url, notice: :logged_out_notice
   end
 end
